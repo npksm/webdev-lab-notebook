c27729f856432383199c35c2717782199b6da963
npksm <nick.pksm@gmail.com>
2025-05-09 12:57:18 -0700

    finished js dom

--- 07-javascript/03-objects.js
+++ 07-javascript/03-objects.js
@@ -7,12 +7,16 @@ const insertItem = (key, value) => {
 
 const deleteItem = (key) => {
   // remove the property with key from result
-  return key;
+  delete result[key];
 };
 
 const lookupItem = (key) => {
   // return the value from result that is associated with key
-  return key;
+
+  if (result.hasOwnProperty(key)) {
+    return result[key];
+  }
+  return "Item does not exist";
 };
 
 const printItems = () => {
--- 07-javascript/04-dom.js
+++ 07-javascript/04-dom.js
@@ -15,3 +15,27 @@
 // add an event listener on the target element
 
 // callback function to handle event
+
+let copyButton = document.querySelector("#copy");
+copyButton.onclick = function (event) {
+  let formInput =
+    document.querySelector("#userInput1").value;
+  let outputArea = document.querySelector(".output");
+  outputArea.append(formInput);
+};
+
+//
+
+let userInput = document.querySelector("#userInput2");
+userInput.addEventListener("input", handleInput);
+
+let newOutput = document.createElement("div");
+newOutput.setAttribute("class", "output");
+document
+  .querySelector("#inputEventExample")
+  .append(newOutput);
+
+function handleInput(event) {
+  console.log("click event", event);
+  newOutput.textContent = userInput2.value;
+}
