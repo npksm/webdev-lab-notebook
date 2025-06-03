c5549faf2f3da5ec5b3c4cff137d1e2debedf312
npksm <nick.pksm@gmail.com>
2025-06-03 09:57:04 -0700

    finished jquery

--- 08-jquery/02-ajax.js
+++ 08-jquery/02-ajax.js
@@ -1,6 +1,50 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+$(document).ready(() => {
+  const url = "https://anapioficeandfire.com/api/books/";
 
-const app = document.querySelector('#books');
+  const addBook = (item) => {
+    $("#books").append(
+      $("<div>")
+        .css({
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "20px",
+        })
+        .append($("<h3>").text(item.name))
+        .append($("<p>").text(item.authors[0]))
+        .append($("<p>").text(item.released.substr(0, 4)))
+        .append(
+          $("<p>").text(`${item.numberOfPages} pages`)
+        )
+    );
+  };
+
+  const fetchData = (url) => {
+    $.ajax({
+      type: "GET",
+      url: url,
+      success: (data) => {
+        //console.log(data);
+        data.forEach((item) => {
+          addBook(item);
+        });
+      },
+      error: (error) => {
+        console.error(error);
+        $("#books")
+          .append("<div>")
+          .text(`An error occurred. Please try again.`);
+      },
+      complete: () => {
+        $("#loading").remove();
+      },
+    });
+  };
+
+  fetchData(url);
+});
+
+/*const app = document.querySelector('#books');
 app.style.paddingLeft = 0;
 const loading = document.querySelector('#loading');
 
@@ -50,3 +94,4 @@ const fetchData = (url) => {
 };
 
 fetchData(url);
+*/
--- reflection.md
+++ reflection.md
@@ -30,8 +30,13 @@ I had actually been playing with a grid area for my form in homework 2 and was r
 
 We had covered a lot of this content in class so it was fairly straightforward. I used the bootstrap website docs and it made this very easy. I thought that the modal would be more complicated, but the example on their site actually worked easily.
 
-### Javascript
+### JavaScript
 
 This was definitely a switch in gears and some of these were pretty tricky.
 Like how CSS felt like a shift from HTML, this was a pretty big shift.
 They were really helpful though and I found them to be great examples of some of the things that are possible with javascript that I wouldn't have thought of, or would have wondered how to do.
+
+### Jquery
+
+Jquery felt like a more complicated JavaScript at first, but once I finished the exercises I saw how useful it is.
+I still think there is a lot more to understand about it, and it will take some more practice to really use it, but this was a great introduction to it.
